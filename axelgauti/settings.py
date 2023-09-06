64a952b80c7fb5169b6d1ca60751fabf61dcc9c0
Axel Gauti <icyaxel@gmail.com>
2023-09-06 15:11:52 +0000

    Now enforcing HTTPS!

--- axelgauti/settings.py
+++ axelgauti/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'axelgauti.middleware.EnforceHttpsMiddleware',
 ]
 
 ROOT_URLCONF = 'axelgauti.urls'
