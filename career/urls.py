a7bc61d56c9596ba8108b2f553bb8ef03b849dd3
Axel Gauti <icyaxel@gmail.com>
2023-09-11 23:44:05 +0000

    Fixed bug with too many curly bracers in a row fucking with heroku

--- career/urls.py
+++ career/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.career_main, name='career_main'),
+    path('test/', views.test_view, name='career_main'),
 ]
--- career/views.py
+++ career/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def career_main(request):
     return render(request, 'career_main.html')
+
+
+from django.http import HttpResponse
+
+def test_view(request):
+    return HttpResponse("Hello, this is a test view!")
