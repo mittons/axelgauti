365a9d36914f7ff1a026dfdcb0a563c75a6a4e61
Axel Gauti <icyaxel@gmail.com>
2025-01-13 05:08:12 +0000

    fixed_

--- career/urls.py
+++ career/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.career_main, name='career_main'),
-    path('old', views.career_main, name='career_main_old'),
+    path('old/', views.career_main_old, name='career_main_old'),
     path('test/', views.test_view, name='career_main'),
 ]
