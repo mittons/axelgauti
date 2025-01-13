3bee53992a3a43bd6aacbdac24a3f7e1f71edbe5
Axel Gauti <icyaxel@gmail.com>
2025-01-13 05:04:28 +0000

    remove malicious nodes

--- career/views.py
+++ career/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def career_main(request):
     return render(request, 'career_main.html')
 
+def career_main_old(request):
+    return render(request, 'career_main_old.html')
+
 
 from django.http import HttpResponse
 
